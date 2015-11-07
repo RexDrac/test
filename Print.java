cd0d1de76d3154299776e998925f815a82c86624
RexDrac <chuanyuyang_charles@foxmail.com>
2015-11-07 22:13:06 +0000

    Update Print.java

--- Print.java
+++ Print.java
@@ -1,5 +1,13 @@
 package CVRP;
+import java.util.ArrayList;
 import java.util.List;
+import java.lang.String;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class Print
 {
@@ -47,5 +55,58 @@ public class Print
     public static void printConfig()
     {
     	
+    }
+    
+    public static void writeResult(String fileName, String loginName, String candidateNum, String name, String description, double lowestCost, List<Integer> bestSolution)
+    {
+        try
+        {
+	    	File Text = new File(fileName);
+	        FileOutputStream fos = new FileOutputStream(Text);
+	        OutputStreamWriter osw = new OutputStreamWriter(fos);    
+	        BufferedWriter w = new BufferedWriter(osw);
+	        w.write("login "+loginName+" "+candidateNum);
+	        w.newLine();
+	        w.write("name "+name);
+	        w.newLine();
+	        w.write(description);
+	        w.newLine();
+	        w.write("cost "+ String.format("%.3f", lowestCost));
+	        w.newLine();
+	        
+	        List<List<Integer>> vehicleRouteList = new ArrayList<List<Integer>>();
+	        List<Integer> depotNodes = new ArrayList<Integer>();
+			//find the position of the depot node in the list
+			for(int i=0;i<bestSolution.size();i++)
+			{
+				if(bestSolution.get(i)==1)
+					depotNodes.add(i);
+			}
+			for(int i=0;i<depotNodes.size()-1;i++)
+			{
+		        List<Integer> vehicleRoute = new ArrayList<Integer>();
+				//System.out.println(depotNodes.get(i));
+				vehicleRoute.addAll(bestSolution.subList(depotNodes.get(i)+1, depotNodes.get(i+1)));
+				//System.out.println(vehicleRoute.size());
+				vehicleRouteList.add(vehicleRoute);
+				//Print.printList(vehicleRouteList.get(i));
+			}
+			for(int i=0;i<vehicleRouteList.size();i++)
+			{
+				//System.out.println(vehicleRouteList.get(i));
+				w.write("1->");
+				for(int j=0;j<vehicleRouteList.get(i).size();j++)
+				{
+					w.write(Integer.toString(vehicleRouteList.get(i).get(j))+"->");
+				}
+				w.write("1");
+				w.newLine();
+			}
+	        w.close();
+        }
+        catch (IOException e)
+        {
+        	System.err.println("Problem writing to the file "+fileName);
+        }    	
     }
 }
